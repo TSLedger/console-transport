e80236ea178674c3fbb27e4972b61f1298b0ab89
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-10-08 15:42:00 -0400

    fix: add jsr to deps.ts datetime fmt

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export type { LedgerTransportOptions, TransportHandleMessage } from 'https://raw.githubusercontent.com/TSLedger/ledger/refs/heads/main/lib/interface/struct.ts';
 export { LedgerTransport } from 'https://raw.githubusercontent.com/TSLedger/ledger/refs/heads/main/lib/transport.ts';
-export { format } from '@std/datetime/format';
+export { format } from 'jsr:@std/datetime/format';
 export * as colors from 'jsr:@std/fmt/colors';
