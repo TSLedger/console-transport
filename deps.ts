8197e79ba7332d84bb5001bdeb4ed6853adf2431
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-10-09 18:42:20 -0400

    fix: add version constraints

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 // Ledger
-export { LedgerTransport, Level } from 'jsr:@ledger/ledger';
-export type { LedgerTransportOptions, TransportHandleMessage } from 'jsr:@ledger/ledger';
+export { LedgerTransport, Level } from 'jsr:@ledger/ledger@0.0.2';
+export type { LedgerTransportOptions, TransportHandleMessage } from 'jsr:@ledger/ledger@0.0.2';
 
 // External
 export { format } from 'jsr:@std/datetime@0.225.2/format';
 export { printf } from 'jsr:@std/fmt@1.0.2/printf';
 export * as colors from 'jsr:@std/fmt@1.0.2/colors';
-export * as stringify from 'npm:string.ify';
+export * as stringify from 'npm:string.ify@1.0.64';
