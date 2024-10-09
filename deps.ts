bdd0b17ff83f213fe42aa47aa2e6855b44d76054
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-10-08 21:32:48 -0400

    feat: sync dev

--- deps.ts
+++ deps.ts
@@ -1,4 +1,7 @@
 export type { LedgerTransportOptions, TransportHandleMessage } from 'https://raw.githubusercontent.com/TSLedger/ledger/refs/heads/main/lib/interface/struct.ts';
+export { Level } from 'https://raw.githubusercontent.com/TSLedger/ledger/refs/heads/main/lib/interface/level.ts';
 export { LedgerTransport } from 'https://raw.githubusercontent.com/TSLedger/ledger/refs/heads/main/lib/transport.ts';
-export { format } from 'jsr:@std/datetime/format';
-export * as colors from 'jsr:@std/fmt/colors';
+export { format } from 'jsr:@std/datetime@0.225.2/format';
+export { printf } from 'jsr:@std/fmt@1.0.2/printf';
+export * as colors from 'jsr:@std/fmt@1.0.2/colors';
+export { serialize } from "https://deno.land/x/serialize@1.1.0/mod.ts";
--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 import type { LedgerTransportOptions, TransportHandleMessage } from './deps.ts';
-import { LedgerTransport } from './deps.ts';
+import { LedgerTransport, Level, printf, serialize } from './deps.ts';
 import { format } from './deps.ts';
 import { colors } from './deps.ts';
 
@@ -10,7 +10,39 @@ export class Transport extends LedgerTransport {
   }
 
   public override async consume(payload: TransportHandleMessage): Promise<void> {
+    // printf
+    let print = '%s %s: %s';
+
+    // Timestamp
     const timestamp = `${colors.gray('[')}${colors.white(format(payload.date, 'yyyy-MM-dd HH:mm:ss.SSS'))}${colors.gray(']')}`;
-    console.info(timestamp);
+
+    // Level
+    let level = Level[payload.level];
+    switch (payload.level) {
+      case Level.TRACE: {
+        level = `${colors.brightCyan(level)}`;
+        break;
+      }
+      case Level.INFO: {
+        level = `${colors.brightBlue(level)}`;
+        break;
+      }
+      case Level.WARN: {
+        level = `${colors.brightYellow(level)}`;
+        break;
+      }
+      case Level.SEVERE: {
+        level = `${colors.brightRed(level)}`;
+        break;
+      }
+    }
+
+    // Message
+    const message = colors.cyan(payload.message);
+
+    // Arguments
+    const args = serialize(payload.args, 2);
+    if (args.trim() !== '[]') print = print + ' %s';
+    printf(`${print}\n`, timestamp, level, message, args);
   }
 }
