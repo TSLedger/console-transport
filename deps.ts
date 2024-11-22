69f3581cc09aea218bf620417576e61cdfc30501
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-11-22 19:59:22 +0000

    chore: update version

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 // Ledger
-export { Level } from 'jsr:@ledger/ledger@0.0.10';
-export type { PageHandler, PageMessageContext, PageOptions } from 'jsr:@ledger/ledger@0.0.10';
+export { Level } from 'jsr:@ledger/ledger@0.0.11';
+export type { PageHandler, PageMessageContext, PageOptions } from 'jsr:@ledger/ledger@0.0.11';
 
 // External
 export { format } from 'jsr:@std/datetime@0.225.2/format';
